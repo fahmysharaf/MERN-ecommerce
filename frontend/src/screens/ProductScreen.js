893fc39bbdecefa6b9ea489ac590c2c61c11edab
fahmysharaf <fahmysharf@gmail.com>
2021-01-19 20:25:36 +0200

    Nodemon, concurrently, .env and esmodules

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -11,7 +11,7 @@ const ProductScreen = ({ match }) => {
       setProduct(data);
     };
     fetchProduct();
-  }, []);
+  }, [match]);
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
